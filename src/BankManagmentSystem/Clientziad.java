c4a198f4c24477c731513a30e2f567d0ecf37906
Zizokimo15 <163547727+Zizokimo15@users.noreply.github.com>
2024-12-09 21:56:44 +0200

    Update Clientziad.java

--- src/BankManagmentSystem/Clientziad.java
+++ src/BankManagmentSystem/Clientziad.java
@@ -1,4 +1,40 @@
 package BankManagmentSystem;
 
-public class Clientziad {
-}
+public class Client {
+        public int accountNumber;
+        public String name;
+        public String contactInfo;
+        public Client(int accountNumber, String name, String contactInfo) {
+            this.accountNumber = accountNumber;
+            this.name = name;
+            this.contactInfo = contactInfo;
+        }
+
+        public void editInfo(String name, String contactInfo) {
+            if (name != null && !name.isEmpty()) {
+                this.name = name;
+            }
+            if (contactInfo != null && !contactInfo.isEmpty()) {
+                this.contactInfo = contactInfo;
+            }
+            System.out.println("Client information updated.");
+        }
+
+        public int getAccountNumber() {
+            return accountNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getContactInfo() {
+            return contactInfo;
+        }
+
+        @Override
+        public String toString() {
+            return "Account Number: " + accountNumber + ", Name: " + name + ", Contact Info: " + contactInfo;
+        }
+    }
+
