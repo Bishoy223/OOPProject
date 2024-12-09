b08612225a36324d9ff83582dbb9627b8b5cba11
Zizokimo15 <163547727+Zizokimo15@users.noreply.github.com>
2024-12-09 21:57:29 +0200

    Update EmployeeZiad.java

--- src/BankManagmentSystem/EmployeeZiad.java
+++ src/BankManagmentSystem/EmployeeZiad.java
@@ -1,4 +1,41 @@
 package BankManagmentSystem;
 
-public class EmployeeZiad {
-}
+public class employee {
+        public String name;
+        public String address;
+        public String position;
+
+        public employee(String name, String address, String position) {
+            this.name = name;
+            this.address = address;
+            this.position = position;
+        }
+
+        public void editInfo(String address, String position) {
+            if (address != null && !address.isEmpty()) {
+                this.address = address;
+            }
+            if (position != null && !position.isEmpty()) {
+                this.position = position;
+            }
+            System.out.println("Employee information updated.");
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public String getPosition() {
+            return position;
+        }
+
+        @Override
+        public String toString() {
+            return "Name: " + name + ", Address: " + address + ", Position: " + position;
+        }
+    }
+
