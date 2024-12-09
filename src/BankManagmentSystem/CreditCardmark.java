0ecd1061fc73dd9af9b444c6d1e0ed512cb9ba49
markfouadz <165848053+markfouadz@users.noreply.github.com>
2024-12-09 21:54:54 +0200

    Update CreditCardmark.java

--- src/BankManagmentSystem/CreditCardmark.java
+++ src/BankManagmentSystem/CreditCardmark.java
@@ -1,4 +1,36 @@
 package BankManagmentSystem;
 
-public class CreditCardmark {
+public class CreditCard {
+    
+    private double limit;
+    private double balance;
+    private int loyaltyPoints;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.balance = limit;
+        this.loyaltyPoints = 0;
+    }
+
+    public void makePayment(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            loyaltyPoints += (int) (amount * 1); // 100 loyalty point per 100 LE
+            System.out.println("Payment successful. Remaining credit: " + balance);
+            System.out.println("Loyalty points earned: " + loyaltyPoints);
+        } else {
+            System.out.println("Insufficient credit limit.");
+        }
+    }
+
+    public void displayCreditCardDetails() {
+        System.out.println("Credit Card Limit: " + limit);
+        System.out.println("Available Balance: " + balance);
+        System.out.println("Loyalty Points: " + loyaltyPoints);
+    }
+
+    public boolean pay(double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pay'");
+    }
 }
